87e275057483896c6b53620b1c54aeea7d921a62
subhajitgolive2023 <subhajitgolive2023@gmail.com>
2023-01-17 15:21:47 +0530

    CI Done

--- test2.py
+++ test2.py
@@ -1,2 +1,3 @@
 print("Hello")
 print("Guys after a long run I am able to integrate the infra of python - VS Code - GIT")
+print("Guys finally I am able to integrate the infra of python - VS Code - GIT - Jenkins")
